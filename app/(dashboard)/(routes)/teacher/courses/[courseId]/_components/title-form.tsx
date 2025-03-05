d3a70495b1654a3aff114aaebd49679dbb721ccb
UnicornDad8 <unicorndad8@gmail.com>
2025-03-05 16:34:10 -0300

    Start TitleForm on CourseDetails component

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { 
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title needs to be at least 1 character long" })
+})
+
+const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course title
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p className="text-sm mt-2">{initialData?.title}</p>
+      )}
+    </div>
+  )
+}
+
+export default TitleForm;
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import TitleForm from "./_components/title-form";
 
 const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
   const { userId } = await auth();
@@ -49,6 +50,10 @@ const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
             <IconBadge icon={LayoutDashboard} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
+          <TitleForm 
+              initialData={course}
+              courseId={course.id}
+          />
         </div>
       </div>
     </div>
