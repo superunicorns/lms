6654fc0d60d768e2ddf2404f0aaa0149741595f4
UnicornDad8 <unicorndad8@gmail.com>
2025-03-05 12:17:42 -0300

    Add badge component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import IconBadge from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
@@ -39,6 +41,14 @@ const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
           <span className="text-sm text-slate-700">
             Complete all fields {completionText}
           </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
         </div>
       </div>
     </div>
