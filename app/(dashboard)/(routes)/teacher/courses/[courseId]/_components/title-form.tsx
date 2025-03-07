b15b8b4339170168257fb5a6e83bdff1205daa71
UnicornDad8 <unicorndad8@gmail.com>
2025-03-07 14:33:52 -0300

    Add input to edit on TitleForm component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -61,6 +61,39 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
       {!isEditing && (
         <p className="text-sm mt-2">{initialData?.title}</p>
       )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField 
+              control={form.control}
+              name="title"
+              render={({field}, index) => (
+                <FormItem key={index}>
+                  <FormControl>
+                    <Input 
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button
+                disabled={!isValid || isSubmitting}
+                type="submit"
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
     </div>
   )
 }
