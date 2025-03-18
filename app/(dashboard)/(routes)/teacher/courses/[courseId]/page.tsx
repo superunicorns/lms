f7796ac682866f03df48c42aa07c43a1c23506c4
UnicornDad8 <unicorndad8@gmail.com>
2025-03-18 08:24:51 -0300

    Category combobox implemented in CourseDetails form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
+import CategoryForm from "./_components/category-form";
 
 const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
   const { userId } = await auth();
@@ -24,7 +25,7 @@ const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
     orderBy: {
       name: "asc"
     }
-  })
+  });
 
   if(!course) {
     return redirect("/");
@@ -69,6 +70,11 @@ const CourseDetails = async ({ params }: { params: { courseId: string }}) => {
           <ImageForm 
               initialData={course}
               courseId={course.id}
+          />
+           <CategoryForm 
+              initialData={course}
+              courseId={course.id}
+              options={categories.map((category) => ({ label: category.name, value: category.id }))}
           />
         </div>
       </div>
